18d35a06299a32358bf8c24b09e6d0f529282ac5
Selvaganapathy-k <selvaganapa000@gmail.com>
2025-10-26 23:17:19 +0530

    first commit

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ import pandas as pd
 import streamlit as st
 import pickle
 from datetime import date
-from sklearn.preprocessing import StandardScaler
 
 # -----------------------------
 # Load model and scaler
